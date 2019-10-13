d93efa9477f55b466fcfb27bf3f4734cb23d606e
lantzmarlene <lantzmarlene@gmail.com>
2019-10-13 20:32:53 +0200

    Practicing More

---
+++ src/com/company/Days/Day8.java
@@ -0,0 +1,27 @@
+package com.company.Days;
+
+import java.util.Scanner;
+
+//Given n names and phone numbers, assemble a phone book that
+// maps friends' names to their respective phone numbers.
+// You will then be given an unknown number of names to query
+// your phone book for. For each name queried, print the associated
+// entry from your phone book on a new line in the form
+// name=phoneNumber; if an entry for name is not found,
+// print Not found instead.
+public class Day8 {
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        for(int i = 0; i<x; i++){
+            String name = scan.next();
+            int phone = scan.nextInt();
+
+        }
+
+        while(scan.hasNext()){
+            String s = scan.next();
+        }
+    }
+}
