10016edb51a6217fb5ca93bc6c2bb23be2cb82d2
lantzmarlene <lantzmarlene@gmail.com>
2019-10-13 20:26:51 +0200

    Finished Day 6

--- src/com/company/Days/Day6.java
+++ src/com/company/Days/Day6.java
@@ -1,30 +1,32 @@
 package com.company.Days;
+//Given a string, s, of length n that is indexed from 0 to n-1,
+// print its even-indexed and odd-indexed characters as 2
+// space-separated strings on a single line
 
 import java.util.Scanner;
 
-//Given an array, A, of N integers, print A's
-//elements in reverse order as a single line of
-//space-separated numbers.
 public class Day6 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Please enter how many items in your array:");
-        int x = scan.nextInt();
-        int[] array = new int[x];
-        String[] arrayItems = scan.nextLine().split(" ");
-        for(int counter = 0; counter<x; counter++){
-            System.out.println("Please enter your value "+(counter +1)+":");
-            array[counter] = Integer.parseInt(scan.next());
+
+        System.out.println("Please input a string: ");
+        String x = scan.nextLine();
+
+        char[] charArray = x.toCharArray();
+
+
+        for (int i = 0; i < charArray.length; i++) {
+            if (i % 2 == 0) {
+                System.out.print(charArray[i]);
+            }
+        }
+        System.out.print("   <-- Even indexed/Odd indexed -->   ");
+        for (int j = 0; j < charArray.length; j++) {
+            if (j % 2 == 1) {
+                System.out.print(charArray[j]);
+            }
         }
         scan.close();
-        System.out.println("Your array in reverse is: ");
-        for(int i = 0; i<x; i++){
-            int index = x - i - 1;
-            System.out.print(array[index]+" ");
-        }
-
-
-
     }
 }
